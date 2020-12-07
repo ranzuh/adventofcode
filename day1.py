cc8ae9b016ca6fc75c4b5b8b8d589713a8977efb
Eetu Rantala <rantala.eetu@gmail.com>
2020-12-08 00:43:46 +0200

    both puzzles in same file

--- day1.py
+++ day1.py
@@ -5,6 +5,16 @@ s = sys.stdin.read()
 s = str(s)
 list = s.splitlines()
 
+
+# puzzle 1
+for i in range(len(list)):
+    for j in list[i:]:
+        if int(list[i]) + int(j) == 2020:
+            print(int(list[i]), "+", int(j), "=", 2020)
+            print("Answer is", int(list[i]) * int(j))
+
+
+# puzzle 2
 for i in range(len(list)):
     for j in range(i, len(list)):
         for k in range(j, len(list)):
--- day2.py
+++ day2.py
@@ -6,8 +6,34 @@ s = sys.stdin.read()
 s = str(s)
 list = s.splitlines()
 
-valids = 0
 
+# puzzle 1
+valids = 0
+for i in list:
+    #split line
+    splits : List[str] = i.split(' ')
+
+    #find bounds
+    minus_index = splits[0].find('-')
+    lower_bound = int(splits[0][:minus_index])
+    higher_bound = int(splits[0][minus_index + 1:])
+
+    #find letter
+    letter = splits[1][0]
+
+    #calculate how many times letter is found in password
+    count = splits[2].count(letter)
+
+    # check if count is in bounds
+    valid = lower_bound <= count <= higher_bound
+
+    if valid: valids += 1
+
+print(valids)
+
+
+# puzzle 2
+valids = 0
 for i in list:
     #split line
     splits : List[str] = i.split(' ')
